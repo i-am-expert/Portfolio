e976bde8fa548978d3b465ecfa7f3884e8629499
i-am-expert <onlyforgithub2708@gmail.com>
2020-06-18 22:21:18 +0530

    Added Footer Component

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -2,18 +2,20 @@ import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// import Intro from './components/Intro';
-// import About from './components/About';
-// import Portfolio from './components/Portfolio';
+import Intro from './components/Intro';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      {/* <Intro />
+      <Intro />
       <About />
-      <Portfolio /> */}
+      <Portfolio />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ portfolio-app/src/components/Footer.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../css/footer.css';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="footer-div container text-center">
+                <div>
+                    <p style={{marginBottom: ".2em"}}><span>Designed & built by Rishabh Sethi</span></p>
+                </div>
+                <div className="footer-icons">
+                    <a class="footer-icon-class hvr-grow" href="https://github.com/i-am-expert"><i class="footer-icon fa fa-github"></i></a>
+                    <a class="footer-icon-class hvr-grow" href="https://www.linkedin.com/in/rishabh-sethi-2708/"><i class="footer-icon fa fa-linkedin"></i></a>
+                    <a class="footer-icon-class hvr-grow" href="https://www.facebook.com/rishabh.sethi.79"><i class="footer-icon fa fa-facebook"></i></a>
+                    <a class="footer-icon-class hvr-grow" href="https://www.instagram.com/rishabhsethi2708/"><i class="footer-icon fa fa-instagram"></i></a>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Footer;
